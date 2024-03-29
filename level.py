6b08e7df7e4f1d3e96c494a2082d26ffb29fc614
Aldin Sabotic <163838218+Carnage12345@users.noreply.github.com>
2024-03-29 13:29:16 +0000

    level.py

---
+++ level.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Level:
+    def _init_(self):
+
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+
+    def run(self):
+
+        pass
